fd453158936111f352a510003ba736858d433f19
gbeldilmi <gbeldilmi@pm.me>
2024-09-29 17:39:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install everything needed
-gem install jekyll webrick
+gem install jekyll webrick erb
 
 printf "Everything installed\n"
 printf "Be sure to add $HOME/.gem/ruby/3.0.0/bin to your PATH\n"
